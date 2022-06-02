ef8f4c27cf6d0d69bc724ee03005450d0814ef14
Nina095 <cedeno.jasmine95@gmail.com>
2022-06-02 00:34:07 +0000

    done

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ window.onload = () => {
   document.querySelector("#btn").addEventListener("click", () => {
     document.querySelector("#excuse").innerHTML = generateExcuse();
   });
-  console.log("Hello Rigo from the console!");
 };
 
 let generateExcuse = () => {
